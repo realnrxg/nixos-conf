93f613fff83bca97bc80722f57d3000938f7c4d0
nrxg <120276921+realnrxg@users.noreply.github.com>
2026-08-04 20:56:41 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,100 +1,92 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# your system.
+# Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, pkgs-stable, ... }:
 
 {
 
-disabledModules = [ "programs/wayland/mango.nix" ];
+  #disabledModules = [ "programs/wayland/mango.nix" ];
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.loader.grub = {
-  enable = true;
-  efiSupport = true;
-  device = "nodev"; # important for UEFI
-};
+    enable = true;
+    efiSupport = true;
+    device = "nodev"; # important for UEFI
+  };
 
-boot.loader.timeout = 1;
+  boot.loader.timeout = 1;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  #IPTABLE
+  # IPTABLE
   boot.kernelModules = [ "iptable_filter" "iptable_nat" "iptable_filter" ];
 
   networking.hostName = "nixosbtw"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain
-
-  #VPN
-  services.mullvad-vpn.enable = true;
+  # VPN
+  services.mullvad-vpn.gui.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
 
-  #docker
+  # Docker
   virtualisation.docker.enable = true;
 
-  #appimage
+  # AppImage
   programs.appimage.enable = true;
-programs.appimage.binfmt = true;
-programs.appimage.package = pkgs.appimage-run.override { extraPkgs = pkgs: [
-pkgs.icu
-pkgs.libxcrypt-legacy
-#pkgs.python312
-#pkgs.python312Packages.torch
-]; };
-
-
-
-
-  #BROWSER
-  xdg.mime = {
-  enable = true;
-  defaultApplications = {
-    "text/html" = "helium.desktop";
-    "x-scheme-handler/http" = "helium.desktop";
-    "x-scheme-handler/https" = "helium.desktop";
-    "x-scheme-handler/about" = "helium.desktop";
-    "x-scheme-handler/unknown" = "helium.desktop";
+  programs.appimage.binfmt = true;
+  programs.appimage.package = pkgs.appimage-run.override {
+    extraPkgs = pkgs: [
+      pkgs.icu
+      pkgs.libxcrypt-legacy
+    ];
   };
-};
 
-#DISK
-fileSystems."/mnt/storage" = {
+  # BROWSER
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "helium.desktop";
+      "x-scheme-handler/http" = "helium.desktop";
+      "x-scheme-handler/https" = "helium.desktop";
+      "x-scheme-handler/about" = "helium.desktop";
+      "x-scheme-handler/unknown" = "helium.desktop";
+    };
+  };
+
+  # DISK
+  fileSystems."/mnt/storage" = {
     device = "/dev/disk/by-uuid/32b9b16a-7fed-446e-ab27-0119977e2391";
     fsType = "ext4";
     options = [ "defaults" "nofail" "x-gvfs-show" ];
   };
 
-  #UDISKS2
+  # UDISKS2
   services.udisks2.enable = true;
 
-  #UDEV
+  # UDEV
   services.udev.extraRules = ''
-  # HyperX Cloud III Wireless (HP Vendor ID 03f0)
-  SUBSYSTEM=="usb", ATTR{idVendor}=="03f0", ATTR{idProduct}=="05b7", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03f0", ATTRS{idProduct}=="05b7", MODE="0660", GROUP="input"
-'';
+    # HyperX Cloud III Wireless (HP Vendor ID 03f0)
+    SUBSYSTEM=="usb", ATTR{idVendor}=="03f0", ATTR{idProduct}=="05b7", MODE="0660", GROUP="input"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03f0", ATTRS{idProduct}=="05b7", MODE="0660", GROUP="input"
+  '';
 
-
-  #HARDWARE UINPUT
+  # HARDWARE UINPUT
   hardware.uinput.enable = true;
 
-  #OPENRAZER
+  # OPENRAZER
   hardware.openrazer.enable = true;
 
-  #NIX LD
+  # NIX LD
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     libGL
@@ -111,10 +103,10 @@ fileSystems."/mnt/storage" = {
   # Enable networking
   networking.networkmanager.enable = true;
   networking.extraHosts = ''
-  127.0.0.1 ubuntu.com
-  127.0.0.1 www.ubuntu.com
-  127.0.0.1 archive.ubuntu.com
-  127.0.0.1 security.ubuntu.com
+    127.0.0.1 ubuntu.com
+    127.0.0.1 www.ubuntu.com
+    127.0.0.1 archive.ubuntu.com
+    127.0.0.1 security.ubuntu.com
   '';
 
   # Set your time zone.
@@ -122,7 +114,6 @@ fileSystems."/mnt/storage" = {
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -135,30 +126,30 @@ fileSystems."/mnt/storage" = {
     LC_TIME = "en_US.UTF-8";
   };
 
-#polkit
-security.polkit.enable = true;
+  # Polkit
+  security.polkit.enable = true;
 
-#sideloading
-services.gvfs.enable = true;
-services.usbmuxd.enable = true;
+  # Sideloading
+  services.gvfs.enable = true;
+  services.usbmuxd.enable = true;
 
-#flatpak
-services.flatpak.enable = true;
+  # Flatpak
+  services.flatpak.enable = true;
 
-#FONTS
-fonts.packages = with pkgs; [
-  nerd-fonts.jetbrains-mono
-  noto-fonts
-  noto-fonts-cjk-sans
-  maple-mono.NF
-];
+  # FONTS
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    maple-mono.NF
+  ];
 
-fonts.fontconfig = {
-  enable = true;
-  defaultFonts = {
-    monospace = [ "Maple Mono NF" ];
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "Maple Mono NF" ];
+    };
   };
-};
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -184,166 +175,146 @@ fonts.fontconfig = {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account.
   users.users.nrxg = {
     isNormalUser = true;
     description = "nrxg";
     extraGroups = [ "networkmanager" "wheel" "plugdev" "openrazer" "audio" "input" "docker" ];
     shell = pkgs.fish;
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    packages = with pkgs; [ ];
   };
 
-  #THUNAR
-    programs.thunar = {
+  # THUNAR
+  programs.thunar = {
     enable = true;
     plugins = with pkgs.xfce; [
       pkgs.thunar-archive-plugin
-      pkgs.thunar-volman #USB AUTOMOUNT
+      pkgs.thunar-volman # USB AUTOMOUNT
     ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-
-#ALLOW INSECURE PACKAGES 
-  nixpkgs.config.permittedInsecurePackages = [
-  "pnpm-10.29.2"
-];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
-  	slurp
-  	mesa-demos
-  	btop
-	neovim
-	vesktop
-	pnpm
-	fish
-	kitty
-	fastfetch
-	glew
-	libGLU
-	zotero
-	steam-run
-	#discord
-	freeglut
-	waybar
-	rofi
-	swaynotificationcenter
-	mesa
-	git
-	perl
-	wget
-	gcc
-	perl
-	sassc
-	wine64Packages.stable
-	winetricks
-	udiskie
-	awww
-	libimobiledevice
-	usbmuxd
-	wl-clipboard
-	ifuse
-	gvfs
-	hyprlock
-	hyprpicker
-	pavucontrol
-	matugen
-	wlogout
-	cava
-	kdePackages.kate
-	unzip
-	unrar
-	flatpak
-	grim
-	imagemagick
-	feh
-	libglvnd
- 	xorg-server
-  	xwayland
-	wayland
-	wayland-protocols
-	gnome-themes-extra
-	glib
-	gsettings-desktop-schemas
-	adwaita-icon-theme
-	qt6Packages.qt6ct
-	libsForQt5.qt5ct
-	adwaita-qt
-	libX11
-	libXcursor
-	libXrandr
-	libXi
-	ani-cli
-	peaclock
-	openrazer-daemon
-	razergenie
-	usbutils
-	playerctl
-	file-roller
-	zip
-	rar
-	manga-tui	
-	wev
-	libGL
-	SDL2
-	SDL2_image
-	SDL2_mixer
-	SDL2_ttf
-	openssl
-	fzf
-	mpv
-	jq
-	obs-studio
-	obsidian
-	lutris
-	vulkan-tools
-	dxvk
-	gamemode
-	p7zip
-	tea
-	galculator
-	opencode
-	tor
-	nftables
-	gradle
-	jdk
-	baobab
-	appimage-run
-	hyprsunset
-	ranger
-	#nodejs_22
-	pkgs-stable.nvibrant
-	libreoffice
-	libnotify
-	yt-dlp
-	vscode
-	cmake
-	gnumake
-	nwg-look
-	
+    slurp
+    mesa-demos
+    btop
+    neovim
+    vesktop
+    pnpm
+    fish
+    kitty
+    fastfetch
+    glew
+    libGLU
+    zotero
+    steam-run
+    freeglut
+    #waybar
+    #rofi
+    #swaynotificationcenter
+    mesa
+    git
+    perl
+    wget
+    gcc
+    sassc
+    wine64Packages.stable
+    winetricks
+    udiskie
+    awww
+    libimobiledevice
+    usbmuxd
+    wl-clipboard
+    ifuse
+    gvfs
+    hyprlock
+    hyprpicker
+    pavucontrol
+    matugen
+    #wlogout
+    cava
+    kdePackages.kate
+    unzip
+    unrar
+    flatpak
+    grim
+    imagemagick
+    feh
+    libglvnd
+    xorg-server
+    xwayland
+    wayland
+    wayland-protocols
+    gnome-themes-extra
+    glib
+    gsettings-desktop-schemas
+    adwaita-icon-theme
+    qt6Packages.qt6ct
+    libsForQt5.qt5ct
+    adwaita-qt
+    libX11
+    libXcursor
+    libXrandr
+    libXi
+    ani-cli
+    peaclock
+    openrazer-daemon
+    razergenie
+    usbutils
+    playerctl
+    file-roller
+    zip
+    rar
+    manga-tui	
+    wev
+    libGL
+    SDL2
+    SDL2_image
+    SDL2_mixer
+    SDL2_ttf
+    openssl
+    fzf
+    mpv
+    jq
+    obs-studio
+    obsidian
+    vulkan-tools
+    dxvk
+    gamemode
+    p7zip
+    tea
+    galculator
+    opencode
+    opencode-desktop
+    tor
+    nftables
+    gradle
+    jdk
+    baobab
+    appimage-run
+    hyprsunset
+    ranger
+    pkgs-stable.nvibrant
+    libreoffice
+    libnotify
+    yt-dlp
+    vscode
+    cmake
+    gnumake
+    nwg-look
+    quickshell
+    material-symbols
 
-	#UPSCAYL
-	(symlinkJoin {
+    # UPSCAYL
+    (symlinkJoin {
       name = "upscayl-wrapped";
       paths = [ upscayl ];
-      nativeBuildInputs = [ makeWrapper ]; # Use nativeBuildInputs for the wrapper tool
+      nativeBuildInputs = [ makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/upscayl \
           --add-flags "--disable-gpu-sandbox" \
@@ -353,105 +324,74 @@ fonts.fontconfig = {
     })
   ];
 
-#FLAKES
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-#fish
-programs.fish = {
-	enable = true;
-	shellAliases = {
-	ff = "fastfetch";
-     };
+  # FISH
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ff = "fastfetch";
+    };
   };
 
-##PORTALS
-xdg.portal = {
+  # PORTALS
+  xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
-      xdg-desktop-portal-wlr
     ];
   };
 
-#HYPRLAND
-programs.hyprland = {
-  enable = true;
-  xwayland.enable = true;
-};
+  # HYPRLAND
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    withUWSM = true;
+  };
 
-#mangowm
-programs.mango.enable = true;
+  # MANGO WM
+  programs.mango.enable = true;
 
-#XWAYLAND ENABLE
-programs.xwayland.enable = true;
+  # XWAYLAND ENABLE
+  programs.xwayland.enable = true;
 
-## NVIDIA
-services.xserver.videoDrivers = [ "nvidia" ];
+  # NVIDIA
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-hardware.graphics = {
-  enable = true;
-  enable32Bit = true;
-  extraPackages = with pkgs; [
-	vulkan-loader
-	vulkan-validation-layers
-  ];
-  extraPackages32 = with pkgs.pkgsi686Linux; [
-  vulkan-loader
-  ];
-};
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      vulkan-loader
+    ];
+  };
 
-hardware.nvidia = {
-  modesetting.enable = true;
-  open = false;
-  nvidiaSettings = true;
-  package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
-};
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+  };
 
-hardware.nvidia-container-toolkit.enable = true;
+  hardware.nvidia-container-toolkit.enable = true;
 
+  # ENVIRONMENT
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    CURSOR_FLAGS = "--no-hardware-cursors";
+  };
 
-#ENVIROMENT
-environment.sessionVariables = {
-  NIXOS_OZONE_WL = "1";
-  MOZ_ENABLE_WAYLAND = "1";
-  LIBVA_DRIVER_NAME = "nvidia";
-  XDG_SESSION_TYPE = "wayland";
-  GBM_BACKEND = "nvidia-drm";
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  CURSOR_FLAGS = "--no-hardware-cursors";
-  #SDL_VIDEODRIVER = "wayland,x11";
-};
+  # DBUS
+  services.dbus.packages = [ pkgs.gsettings-desktop-schemas ];
 
-#DBUS
-services.dbus.packages = [ pkgs.gsettings-desktop-schemas ];
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
